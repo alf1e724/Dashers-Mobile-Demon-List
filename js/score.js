03069663d9e85ea03e984e43b20ac4d29cb6c209
smegful dashers <xmollyxgood@gmail.com>
2026-03-12 20:36:08 +0000

    Initial commit

---
+++ js/score.js
@@ -0,0 +1,53 @@
+/**
+ * Numbers of decimal digits to round to
+ */
+const scale = 3;
+
+/**
+ * Calculate the score awarded when having a certain percentage on a list level
+ * @param {Number} rank Position on the list
+ * @param {Number} percent Percentage of completion
+ * @param {Number} minPercent Minimum percentage required
+ * @returns {Number}
+ */
+export function score(rank, percent, minPercent) {
+    if (rank > 150) {
+        return 0;
+    }
+    if (rank > 75 && percent < 100) {
+        return 0;
+    }
+
+    // Old formula
+    /*
+    let score = (rank <= 100) ? Math.pow(350, Math.pow(0.97, rank-1)) : 0;
+
+    */
+    // New formula
+    let score = (rank <= 100) ? 350 * Math.pow(0.9715028078, rank-1) : 0;
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
+}
+
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
+}
